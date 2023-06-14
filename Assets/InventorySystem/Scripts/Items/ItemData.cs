f0a673bcb109075b97713a44ec1d98787181d58f
Jasper Honkasalo <japsu.honkasalo@gmail.com>
2023-06-14 07:12:20 +0100

    Removed unnecessary files and started from scratch.

---
+++ Assets/InventorySystem/Scripts/Items/ItemData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace SourceData.Scripts
+{
+    /// <summary>
+    /// Data-asset for an item.
+    /// </summary>
+    [CreateAssetMenu(fileName = "ItData_", menuName = "Items/New Item", order = 0)]
+    public class ItemData : ScriptableObject
+    {
+        [SerializeField] private string _itemName = "MissingYes";
+        [SerializeField] private string _itemDescription = "Missing Description";
+        [SerializeField] private Sprite itemSprite;
+        [SerializeField] private InventorySize _inventorySize = new(1, 1);
+
+        public int HashId => GetHashCode();
+        public string Name => _itemName;
+        public Sprite Sprite => itemSprite;
+        public string Description => _itemDescription;
+        public InventorySize InventorySize => _inventorySize;
+    }
+
+
+    [Serializable]
+    public struct InventorySize
+    {
+        [Min(1)]
+        public int X;
+        
+        [Min(1)]
+        public int Y;
+
+
+        public InventorySize(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Assets/InventorySystem/Scripts/Items/ItemStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace SourceData.Scripts
+{
+    /// <summary>
+    /// Represents a certain amount of <see cref="ItemData"/>.
+    /// </summary>
+    [Serializable]
+    public struct ItemStack
+    {
+        [SerializeField] private ItemData _item;
+        [SerializeField, Min(1)] private int _count;
+        
+        public int Id => _item.HashId;
+        public ItemData Item => _item;
+        public int Count => _count;
+        public bool IsEmpty => Count < 1;
+        
+
+        public ItemStack(ItemData item, int count = 1)
+        {
+            _item = item;
+            _count = count;
+        }
+
+
+        public static ItemStack operator +(ItemStack a, ItemStack b)
+        {
+            return new ItemStack(a.Item, a.Count + b.Count);
+        }
+
+
+        public static ItemStack operator -(ItemStack a, ItemStack b)
+        {
+            return new ItemStack(a.Item, a.Count - b.Count);
+        }
+
+
+        public void SpawnAsPickup(Vector2 position)
+        {
+            throw new NotImplementedException();
+            //ItemStackPickup pickup = Object.Instantiate(PrefabReferences.ItemPickupPrefab, position, Quaternion.identity);
+            //pickup.Initialize(this);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Count} x {Item.Name}";
+        }
+    }
+}
