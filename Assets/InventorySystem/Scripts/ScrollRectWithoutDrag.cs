0d94a6e47a93124455d590000830b4bb8da72d3d
Jasper Honkasalo <japsu.honkasalo@gmail.com>
2023-06-19 10:08:25 +0100

    Working ScrollRect with dynamic sized content entities.

---
+++ Assets/InventorySystem/Scripts/ScrollRectWithoutDrag.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace InventorySystem
+{
+    public class ScrollRectWithoutDrag : ScrollRect {
+        public override void OnBeginDrag(PointerEventData eventData) { }
+        public override void OnDrag(PointerEventData eventData) { }
+        public override void OnEndDrag(PointerEventData eventData) { }
+    }
+}
